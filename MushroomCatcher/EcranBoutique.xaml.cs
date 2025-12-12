edc24b99afe765b6e4fedf15512e069685cf8c2a
DeMeoLucas2 <Lucas.De-Meo@etu.univ-smb.fr>
2025-12-12 09:41:19 +0100

    Initialisation des ercran de boutique et de vente avec les bouttons d'accès et de retour fonctionnel

---
+++ MushroomCatcher/EcranBoutique.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MushroomCatcher
+{
+    /// <summary>
+    /// Logique d'interaction pour EcranBoutique.xaml
+    /// </summary>
+    public partial class EcranBoutique : UserControl
+    {
+        public EcranBoutique()
+        {
+            InitializeComponent();
+        }
+
+        private void ButRetourBoutique_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButVendre_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
--- MushroomCatcher/MainWindow.xaml.cs
+++ MushroomCatcher/MainWindow.xaml.cs
@@ -64,8 +64,40 @@ namespace MushroomCatcher
 
         private void AfficheJeu(object sender, RoutedEventArgs e)
         {
-            JEU uc = new JEU(); // crée et charge l'écran de démarrage
+            JEU uc = new JEU(); // crée et charge l'écran de Jeu
             ZoneJeu.Content = uc; // associe l'écran au conteneur
+            // uc.ButBoutique.Click += AfficheBoutique; // affiche l'écran de la boutique
         }
+
+        private void AfficheRetourJeu(object sender, RoutedEventArgs e)
+        {
+            JEU uc = new JEU(); // crée et charge l'écran de Jeu
+            ZoneJeu.Content = uc; // associe l'écran au conteneur
+            // uc.ButBoutique.Click += AfficheBoutique; // affiche l'écran de la boutique
+        }
+
+        private void AfficheBoutique(object sender, RoutedEventArgs e)
+        {
+            EcranBoutique uc = new EcranBoutique(); // crée et charge l'écran de la boutique
+            ZoneJeu.Content = uc; // associe l'écran au conteneur
+            uc.ButRetourBoutique.Click += AfficheRetourJeu; // retourne à l'écran de jeu
+            uc.ButVendre.Click += AfficheVente; // affiche l'écran de vente
+        }
+
+        private void AfficheRetourBoutique(object sender, RoutedEventArgs e)
+        {
+            EcranBoutique uc = new EcranBoutique(); // crée et charge l'écran de la boutique
+            ZoneJeu.Content = uc; // associe l'écran au conteneur
+            uc.ButRetourBoutique.Click += AfficheRetourJeu; // retourne à l'écran de jeu
+            uc.ButVendre.Click += AfficheVente; // affiche l'écran de vente
+        }
+
+        private void AfficheVente(object sender, RoutedEventArgs e)
+        {
+            EcranBoutique uc = new EcranBoutique(); // crée et charge l'écran de vente
+            ZoneJeu.Content = uc; // associe l'écran au conteneur
+            uc.ButVendre.Click += AfficheRetourBoutique; // retourne à l'écran de boutique
+        }
+        
     }
 }
